8d85a1673854ca0addab4817508fd70cb4a08a2b
romanshrestha20 <stha.roman20@gmail.com>
2026-08-06 00:07:14 +0300

    feat: add Prisma module and service for database interaction

---
+++ apps/api/src/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, OnApplicationShutdown } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@restaurant/database/generated';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnApplicationShutdown
+{
+  constructor(config: ConfigService) {
+    const adapter = new PrismaPg({
+      connectionString: config.getOrThrow<string>('DATABASE_URL'),
+    });
+
+    super({ adapter });
+  }
+
+  async onApplicationShutdown(): Promise<void> {
+    await this.$disconnect();
+  }
+}
