92e06b7a5cc3170b58fbd1e560bc5167cbef1ba1
romanshrestha20 <stha.roman20@gmail.com>
2026-08-09 14:42:30 +0300

    feat: add throttling functionality with ThrottlerGuard in AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { PrismaModule } from './prisma/prisma.module';
 import { envValidationSchema } from './config/env';
 import { HealthModule } from './modules/health/health.module';
@@ -13,9 +15,21 @@ import { AuthModule } from './modules/auth/auth.module';
       cache: true,
       validationSchema: envValidationSchema,
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60_000,
+        limit: 100,
+      },
+    ]),
     PrismaModule,
     HealthModule,
     AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class AppModule {}
