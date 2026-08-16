d4fdebd5b13efbd2259a746f534d27c5d9c2429b
romanshrestha20 <stha.roman20@gmail.com>
2026-08-17 01:21:34 +0300

    feat: add MenuModule to CartModule and OrdersModule imports

--- apps/api/src/modules/cart/cart.module.ts
+++ apps/api/src/modules/cart/cart.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { MenuModule } from '../menu/menu.module';
 import { CartController } from './cart.controller';
 import { CartService } from './cart.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, MenuModule],
   controllers: [CartController],
   providers: [CartService],
+  exports: [CartService],
 })
 export class CartModule {}
+
--- apps/api/src/modules/orders/orders.module.ts
+++ apps/api/src/modules/orders/orders.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { RealtimeModule } from '../../common/realtime/realtime.module';
+import { MenuModule } from '../menu/menu.module';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
 @Module({
-  imports: [PrismaModule, RealtimeModule],
+  imports: [PrismaModule, RealtimeModule, MenuModule],
   controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
 })
 export class OrdersModule {}
+
